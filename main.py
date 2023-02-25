88e5a43a4213b4a7bd6073ad27bb1c1d73f72bb7
Alex <a33211noskov@gmail.com>
2023-02-25 11:34:05 +0200

    Lesson 25.02

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_excel("Working_example.xlsx", header=1)
-df2 = pd.read_excel("stratigraphy_Bairak35.xlsx", header=1)
+df2 = pd.read_excel("stratigraphy_Bairak35.xlsx")
 #df = df[df['Т150'] > 1000]
 #print(df)
 
@@ -13,7 +15,7 @@ Vint_list = []
 #Vint = DZ/DT*2000
 wt_increment = 2000
 
-#time_increment = input('Please specify if time column is TWT format. Entrer True/False')
+#time_increment = input('Please specify if time column is TWT format. Enter True/False')
 # if time_increment = True:
 #     wt_increment = 2000
 # else:
@@ -40,11 +42,26 @@ Vint_list.insert(0,0)
 df['DZ'] = DZ_list
 df['DT'] = DT_list
 df['Vint'] = Vint_list
-print(df2.describe())
+df['smooth_3'] = df['Vint'].rolling(window=3).mean()
+df['smooth_5'] = df['Vint'].rolling(window=5).mean()
+df['exponential'] = df['Vint'].ewm(com=0.5).mean()#to test more
+filter5 = np.convolve(np.ones(5), np.ones(5), mode='full')
+filter5 = filter5/sum(filter5)
+filter7 = np.convolve(np.ones(7), np.ones(7), mode='full')
+filter7 = filter7/sum(filter7)
+df['filtered'] = np.convolve(df['Vint'], filter7, mode='same')
+print(df2)
 #df.to_excel('test.xlsx')
 
 #smoothing x3, x5, triangular filtring
-#structure hints for solo work
 
 
 
+
+plt.plot(df['Vint'],df['Hкаб.'])
+# plt.plot(df['smooth_3'],df['Hкаб.'])
+# plt.plot(df['smooth_5'],df['Hкаб.'])
+plt.plot(df['filtered'],df['Hкаб.'])
+plt.plot(df2['MD'])
+plt.gca().invert_yaxis()
+plt.show()
